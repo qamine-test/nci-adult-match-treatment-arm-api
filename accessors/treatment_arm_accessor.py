e1cbf3560d7eddc76f326ce3d769a71f12be2321
Matt Smith <matt.smith2@nih.gov>
2017-05-16 15:26:45 -0400

    Now accesses new treatmentArms collection instead of treatmentArm collection.

--- accessors/treatment_arm_accessor.py
+++ accessors/treatment_arm_accessor.py
@@ -3,23 +3,23 @@ from bson import json_util
 import json
 
 
-class TreatmentArmAccessor(MongoDbAccessor):
+class TreatmentArmsAccessor(MongoDbAccessor):
     """
     The TreatmentArm data accessor
     """
 
     def __init__(self):
-        MongoDbAccessor.__init__(self, 'treatmentArm')
+        MongoDbAccessor.__init__(self, 'treatmentArms')
 
     def find(self, query, projection):
         """
         Returns items from the collection using a query and a projection
         """
 
-        res = []
-        for doc in self.collection.find(query, projection):
-            res.append(json.loads(json_util.dumps(doc)))
-
+        # res = []
+        # for doc in self.collection.find(query, projection):
+        #     res.append(json.loads(json_util.dumps(doc)))
+        res = [json.loads(json_util.dumps(doc)) for doc in self.collection.find(query, projection)]
         return res
 
     def find_one(self, query, projection):
