d7f1d9bea3822485da4f06583df1774af90d66c5
Matt Smith <matt.smith2@nih.gov>
2017-05-17 14:41:23 -0400

    Removed call to jsonify; flask_restful Resource automatically converts to JSON.

--- resources/version.py
+++ resources/version.py
@@ -3,7 +3,6 @@ The API Version
 """
 
 from flask_restful import Resource
-from flask.json import jsonify
 
 class Version(Resource):
     """
@@ -16,4 +15,4 @@ class Version(Resource):
         Gets the Version resource
         """
 
-        return jsonify({"version": "0.0.1"})
+        return {"version": "0.0.1"}
