f7f1bd6736a4173e52aa1ec0066b4b24cc068396
Matt Smith <matt.smith2@nih.gov>
2017-05-23 10:12:22 -0400

    Preparing unit test script for amois.py; currently does nothing.

--- tests/test_amois.py
+++ tests/test_amois.py
@@ -1,9 +1,15 @@
 import unittest
 
+# import flask
+from ddt import ddt  # , data, unpack
+# from mock import patch
+# from resources.amois import AmoisAnnotator
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+@ddt
+class TestAmoisAnnotator(unittest.TestCase):
+    def test_get(self):
+        pass
 
 
 if __name__ == '__main__':
