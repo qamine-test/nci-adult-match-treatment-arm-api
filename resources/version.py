912b4a48dd8cdf1cbd3095a48e243a07fc6d1ad0
Matt Smith <matt.smith2@nih.gov>
2017-05-31 18:01:39 -0400

    Made more robust to return information from a version.json file.  Added try/except blocks.

--- resources/version.py
+++ resources/version.py
@@ -2,17 +2,32 @@
 The API Version
 """
 
+import logging
+
+import flask
 from flask_restful import Resource
+
 
 class Version(Resource):
     """
     Version resource
     """
+    def __init__(self, filename='version.json'):
+        self.logger = logging.getLogger(__name__)
+        self.filename = filename
 
-    @classmethod
-    def get(cls):
+    def get(self):
         """
         Gets the Version resource
         """
+        self.logger.debug('Retrieving API Version')
 
-        return {"version": "0.0.1"}
+        try:
+            with flask.current_app.open_resource(self.filename) as f:
+                json_data = flask.json.load(f)
+                return json_data
+
+        except Exception as ex:
+            message = str(ex)
+            self.logger.exception(message)
+            return {"version": "0.0.2", "build_number": "None"}
