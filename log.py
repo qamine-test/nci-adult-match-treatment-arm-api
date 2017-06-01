aa9117c9b1263f46593d3fe6d07d4dc7f55c6990
Matt Smith <matt.smith2@nih.gov>
2017-06-01 09:29:29 -0400

    Added common log configuration to standardize format of logs.

---
+++ log.py
@@ -0,0 +1,32 @@
+"""
+Logging configuration
+"""
+
+import logging.config
+
+study_id = 'EAY131'
+
+LOGGER_CONFIGURATION = {
+    'version': 1,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s - %(levelname)s - TREATMENT_ARM_API ['+study_id+'] - %(name)s.%(module)s.%(funcName)s:%(lineno)d - %(message)s',
+        },
+    },
+    'handlers': {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "standard",
+            "stream": "ext://sys.stdout"
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level':'DEBUG',
+        }
+    }
+}
+
+logging.config.dictConfig(LOGGER_CONFIGURATION)
