15724df44e3f6985bfd38bebdb5863e6984c59b3
Matt Smith <matt.smith2@nih.gov>
2017-06-07 11:35:59 -0400

    Uses parent class implementations of find, find_one, count, and aggregate.

--- accessors/patient_accessor.py
+++ accessors/patient_accessor.py
@@ -2,10 +2,10 @@
 Patient MongoDB Accessor
 """
 
-from accessors.mongo_db_accessor import MongoDbAccessor
-from bson import json_util
-import json
 import logging
+
+from accessors.mongo_db_accessor import MongoDbAccessor
+
 
 class PatientAccessor(MongoDbAccessor):
     """
@@ -16,17 +16,30 @@ class PatientAccessor(MongoDbAccessor):
         MongoDbAccessor.__init__(self, 'patient')
         self.logger = logging.getLogger(__name__)
 
-    def find(self, query, projection):
+   def find(self, query, projection):
         """
-        Returns items from the collection using a query and a projection
+        Returns items from the collection using a query and a projection.
         """
-        self.logger.debug('Retrieving Patients from database')
-        return [json.loads(json_util.dumps(doc)) for doc in self.collection.find(query, projection)]
+        self.logger.debug('Retrieving Patient documents from database')
+        return MongoDbAccessor.find(self, query, projection)
 
     def find_one(self, query, projection):
         """
         Returns one element found by filter
         """
-        self.logger.debug('Retrieving one Patient from database')
-        return json.loads(json_util.dumps(
-            self.collection.find_one(query, projection)))
+        self.logger.debug('Retrieving one Patient document from database')
+        return MongoDbAccessor.find_one(self, query, projection)
+
+    def count(self, query):
+        """
+        Returns the number of items from the collection using a query.
+        """
+        self.logger.debug('Counting Patient documents in database')
+        return MongoDbAccessor.count(self, query)
+
+    def aggregate(self, pipeline):
+        """
+        Returns the aggregation defined by pipeline.
+        """
+        self.logger.debug('Retrieving Patient document aggregation from database')
+        return MongoDbAccessor.aggregate(self, pipeline)
