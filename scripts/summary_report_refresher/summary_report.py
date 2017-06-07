7b3cb48e8c10e5c189f1e9e906fff7f84a0a3c1e
Matt Smith <matt.smith2@nih.gov>
2017-06-07 12:07:38 -0400

    Changed internal layout.

--- scripts/summary_report_refresher/summary_report.py
+++ scripts/summary_report_refresher/summary_report.py
@@ -2,41 +2,44 @@
 
 class SummaryReport:
     # Patient Type Constants
-    NOT_ENROLLED = 'numNotEnrolledPatients'
+    NOT_ENROLLED = 'numNotEnrolledPatient'
     FORMER = 'numFormerPatients'
     PENDING = 'numPendingArmApproval'
     CURRENT = 'numCurrentPatientsOnArm'
+    ASSNMNT_RECS = 'assignmentRecords'
 
     _SR_COUNT_FIELDS = [NOT_ENROLLED, FORMER, CURRENT, PENDING]
-    _REQ_JSON_FIELDS = ['_id', 'treatmentId', 'version', 'treatmentArmStatus', 'summaryReport']
+    _REQ_JSON_FIELDS = ['_id', 'treatmentId', 'version', 'treatmentArmStatus']
     _REQ_SR_FIELDS = _SR_COUNT_FIELDS + ['assignmentRecords']
 
     @staticmethod
-    def _validate_sr_json_doc(sr_json_doc):
-        if sr_json_doc is None:
-            sr_json_doc = dict()
-        missing_fields = [f for f in SummaryReport._REQ_JSON_FIELDS if f not in sr_json_doc]
-        sr = sr_json_doc['summaryReport'] if 'summaryReport' in sr_json_doc else dict()
-        missing_fields += ['summaryReport.'+f for f in SummaryReport._REQ_SR_FIELDS if f not in sr]
+    def _validate_sr_json_doc(ta_json_doc):
+        if ta_json_doc is None:
+            ta_json_doc = dict()
+        missing_fields = [f for f in SummaryReport._REQ_JSON_FIELDS if f not in ta_json_doc]
+        # sr = ta_json_doc['summaryReport'] if 'summaryReport' in ta_json_doc else dict()
+        # missing_fields += ['summaryReport.'+f for f in SummaryReport._REQ_SR_FIELDS if f not in sr]
 
         if missing_fields:
             err_msg = ("The following required fields were missing from the submitted summary report JSON document: "
                        + ", ".join(missing_fields))
             raise Exception(err_msg)
 
-    def __init__(self, sr_json_doc):
+    def __init__(self, ta_json_doc):
         """
 
-        :param sr_json_doc:
+        :param ta_json_doc:
         """
-        SummaryReport._validate_sr_json_doc(sr_json_doc)
-        self.sr = sr_json_doc
+        SummaryReport._validate_sr_json_doc(ta_json_doc)
+        self.ta = ta_json_doc
+        self.sr = dict([(f, 0) for f in SummaryReport._SR_COUNT_FIELDS])
+        self.sr[SummaryReport.ASSNMNT_RECS] = []
 
     def __getattr__(self, item):
         if item in SummaryReport._REQ_SR_FIELDS:
-            return self.sr['summaryReport'][item]
-        else:
             return self.sr[item]
+        else:
+            return self.ta[item]
 
     def add_patient_by_type(self, pat_type):
         """
@@ -46,5 +49,4 @@ class SummaryReport:
         """
         if pat_type not in SummaryReport._SR_COUNT_FIELDS:
             raise Exception("Invalid patient type '{t}'".format(t=pat_type))
-        self.sr['summaryReport'][pat_type] += 1
-
+        self.sr[pat_type] += 1
