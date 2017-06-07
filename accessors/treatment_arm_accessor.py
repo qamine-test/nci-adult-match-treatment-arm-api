befb0bd8d05d106923e02068ab48c354e7de03c7
Matt Smith <matt.smith2@nih.gov>
2017-06-07 08:22:34 -0400

    Added logging statements.

--- accessors/treatment_arm_accessor.py
+++ accessors/treatment_arm_accessor.py
@@ -1,6 +1,9 @@
-from accessors.mongo_db_accessor import MongoDbAccessor
-from bson import json_util
 import json
+import logging
+
+from bson import json_util
+
+from accessors.mongo_db_accessor import MongoDbAccessor
 
 
 class TreatmentArmsAccessor(MongoDbAccessor):
@@ -10,17 +13,20 @@ class TreatmentArmsAccessor(MongoDbAccessor):
 
     def __init__(self):
         MongoDbAccessor.__init__(self, 'treatmentArms')
+        self.logger = logging.getLogger(__name__)
 
     def find(self, query, projection):
         """
         Returns items from the collection using a query and a projection.
         """
+        self.logger.debug('Retrieving TreatmentArms documents from database')
         return [json.loads(json_util.dumps(doc)) for doc in self.collection.find(query, projection)]
 
     def find_one(self, query, projection):
         """
         Returns one element found by filter
         """
+        self.logger.debug('Retrieving one TreatmentArms document from database')
         return json.loads(json_util.dumps(
             self.collection.find_one(query, projection)))
 
@@ -28,15 +34,18 @@ class TreatmentArmsAccessor(MongoDbAccessor):
         """
         Returns the number of items from the collection using a query.
         """
+        self.logger.debug('Counting TreatmentArms documents in database')
         return self.collection.count(query)
 
     def aggregate(self, pipeline):
         """
         Returns the aggregation defined by pipeline.
         """
+        self.logger.debug('Retrieving TreatmentArms document aggregation from database')
         return self.collection.aggregate(pipeline)
 
     def get_ta_non_hotspot_rules(self):
+        self.logger.debug('Retrieving TreatmentArms non-Hotspot Rules from database')
         return [ta_nhr for ta_nhr in self.aggregate([
             {"$match": {"variantReport.nonHotspotRules": {"$ne": []}}},
             {"$unwind": "$variantReport.nonHotspotRules"},
@@ -55,6 +64,7 @@ class TreatmentArmsAccessor(MongoDbAccessor):
 
 
     def get_ta_identifier_rules(self, variant_type):
+        self.logger.debug('Retrieving TreatmentArms Hotspot Rules from database')
         if variant_type not in ['singleNucleotideVariants', 'copyNumberVariants', 'geneFusions', 'indels']:
             raise Exception( "Unknown variant_type '%s' passed to %s" % (variant_type, instance.__class__.__name__))
 
