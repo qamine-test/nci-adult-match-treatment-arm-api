e0643531dccd5b045b5caa983dd6c87dd07018be
Matt Smith <matt.smith2@nih.gov>
2017-06-07 12:10:55 -0400

    Moved query/projection for TA data to TreatmentArmsAccessor class.  Added and called an incomplete implementation of _update_summary_report method (currently only gets the patients).

--- scripts/summary_report_refresher/refresher.py
+++ scripts/summary_report_refresher/refresher.py
@@ -1,17 +1,26 @@
 import logging
 
+from accessors.patient_accessor import PatientAccessor
 from accessors.treatment_arm_accessor import TreatmentArmsAccessor
+from summary_report import SummaryReport
+
 
 class Refresher:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        self.summary_rpts = TreatmentArmsAccessor().find({'dateArchived': None},
-                                                         {'treatmentId': 1,
-                                                          'version': 1,
-                                                          'summaryReport': 1,
-                                                          'dateArchived': 1,
-                                                          'treatmentArmStatus': 1,
-                                                          'stateToken': 1})
+        self.pat_accessor = PatientAccessor()
+        self.ta_accessor = TreatmentArmsAccessor()
+        self.summary_rpts = [SummaryReport(ta_data)
+                             for ta_data in self.ta_accessor.get_arms_for_summary_report_refresh()]
 
     def run(self):
         self.logger.info("{cnt} summary reports selected for update".format(cnt=len(self.summary_rpts)))
+        for sr in self.summary_rpts:
+            self._update_summary_report(sr)
+
+    def _update_summary_report(self, sum_rpt):
+        patients = self.pat_accessor.get_patients_by_treatment_arm_id(sum_rpt.treatmentId)
+        self.logger.debug("{cnt} patients returned for '{trtmt_id}"
+                          .format(cnt=len(patients), trtmt_id=sum_rpt.treatmentId))
+
+    # def
