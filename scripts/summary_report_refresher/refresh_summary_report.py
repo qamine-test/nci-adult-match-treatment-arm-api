dc1275d6f2c8b6ed95481fda8a76c012a703a04f
Matt Smith <matt.smith2@nih.gov>
2017-06-08 14:01:10 -0400

    Changed database name from 'match' to 'Match'

--- scripts/summary_report_refresher/refresh_summary_report.py
+++ scripts/summary_report_refresher/refresh_summary_report.py
@@ -15,7 +15,7 @@ class Configuration(metaclass=MetaFlaskEnv):
     """
     DEBUG = True
     PORT = 5010
-    MONGODB_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/match')
+    MONGODB_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Match')
     # Some instances of the DB are named 'Match' and others 'match'.
     DB_NAME = 'match' if '/match' in MONGODB_URI else 'Match'
 
