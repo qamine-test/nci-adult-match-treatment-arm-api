487f0724f4f1e247168632931875dfb614515118
Matt Smith <matt.smith2@nih.gov>
2017-06-08 14:00:17 -0400

    Now calls the TreatemenArmAccessor.update_summary_report method.  Started implementing _create_assignment_record method (not yet being called).

--- scripts/summary_report_refresher/refresher.py
+++ scripts/summary_report_refresher/refresher.py
@@ -1,7 +1,9 @@
 import logging
+from datetime import datetime
 
 from accessors.patient_accessor import PatientAccessor
 from accessors.treatment_arm_accessor import TreatmentArmsAccessor
+from scripts.summary_report_refresher.assignment_record import AssignmentRecord
 from scripts.summary_report_refresher.summary_report import SummaryReport
 
 
@@ -28,8 +30,10 @@ class Refresher:
         for pat in patients:
             Refresher._match(pat, sum_rpt)
 
-        # TODO not implemented yet
-        # self.ta_accessor.update_summary_report(sum_rpt)
+        import pprint
+        pprint.pprint(sum_rpt.sr)
+        pprint.pprint(sum_rpt.ta)
+        self.ta_accessor.update_summary_report(sum_rpt._id, sum_rpt.get_json())
 
     @staticmethod
     def _match(pat, sum_rpt):
@@ -38,3 +42,20 @@ class Refresher:
             sum_rpt.add_patient_by_type(SummaryReport.CURRENT)
         elif patient_status == 'PENDING_APPROVAL':
             sum_rpt.add_patient_by_type(SummaryReport.PENDING)
+
+    # def __init__(self, pat_seq_num, ta_version, assnmnt_status, assnmnt_reason, step_num, diseases, analysis_id,
+    #              date_selected, date_on_arm, date_off_arm=None):
+    @staticmethod
+    def _create_assignment_record(pat):
+
+        return AssignmentRecord(pat['patientSequenceNumber'],
+                                pat['treatmentArm']['version'],
+                                pat[PATIENT_STATUS_FIELD],
+                                "todo: get reason",  # TODO look in pat['patientAssignments']['patientAssignmentLogic']
+                                pat['currentStepNumber'],
+                                pat['diseases'],
+                                "todo: analysisId",  # TODO find out where this comes from
+                                datetime(2001,1,1),  # TODO get dateSelected from patientTriggers
+                                datetime(2001, 1, 1),  # TODO get dateOnArm from patientTriggers
+                                None,  # TODO get dateOffArm
+                                )
