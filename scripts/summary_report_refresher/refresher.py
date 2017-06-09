3700dca0a9594b1194a49ee082d2ff5da54b4c0a
Matt Smith <matt.smith2@nih.gov>
2017-06-09 16:11:05 -0400

    Implemented identification of Former patients and patient considered but Not Enrolled.

--- scripts/summary_report_refresher/refresher.py
+++ scripts/summary_report_refresher/refresher.py
@@ -12,6 +12,15 @@ PATIENT_STATUS_FIELD = 'currentPatientStatus'
 
 
 class Refresher:
+    OTHER_STATUSES = [
+        'OFF_TRIAL_NO_TA_AVAILABLE',
+        'OFF_TRIAL_DECEASED',
+        'OFF_TRIAL',
+        'OFF_TRIAL_BIOPSY_EXPIRED',
+        'OFF_TRIAL_NOT_CONSENTED',
+        'REJOIN_REQUESTED',
+    ]
+
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.pat_accessor = PatientAccessor()
@@ -25,51 +34,77 @@ class Refresher:
             self._update_summary_report(sr)
 
     def _update_summary_report(self, sum_rpt):
+        """
+        Update the given summary report with counts and assignment records.
+        :param sum_rpt: The summary report of a treatment that requires updating.
+        """
+        # Get all patients associated with the Treatment Arm of the given Summary Report
         patients = [Patient(p) for p in self.pat_accessor.get_patients_by_treatment_arm_id(sum_rpt.treatmentId)]
         self.logger.debug("{cnt} patients returned for '{trtmt_id}"
                           .format(cnt=len(patients), trtmt_id=sum_rpt.treatmentId))
 
+        # Update the summary report for any patients that meet the criteria.
         for pat in patients:
             Refresher._match(pat, sum_rpt)
 
-        import pprint
-        pprint.pprint(sum_rpt.sr)
-        pprint.pprint(sum_rpt.ta)
+        # import pprint
+        # pprint.pprint(sum_rpt.sr)
+        # pprint.pprint(sum_rpt.ta)
+
+        # Update the summary report in the treatmentArms collection on the database
         self.ta_accessor.update_summary_report(sum_rpt._id, sum_rpt.get_json())
 
     @staticmethod
-    def _match(pat, sum_rpt):
-        patient_status = pat.currentPatientStatus
+    def _match(patient, sum_rpt):
+        patient_type = Refresher._determine_patient_classification(patient)
+
+        if patient_type is not None:
+            assignment_rec = Refresher._create_assignment_record(patient, sum_rpt.treatmentId)
+            sum_rpt.add_patient_by_type(patient_type, assignment_rec)
+
+    @staticmethod
+    def _determine_patient_classification(patient):
+        """
+        Determines if patient is a patient currently on the the Treatment Arm, pending for the Treatment Arm,
+        formerly on the Treatment Arm, or once considered but not enrolled on the Treatment Arm.
+        :param patient: a Patient
+        :return: SummaryReport.CURRENT, SummaryReport.PENDING, SummaryReport.FORMER, SummaryReport.NOT_ENROLLED, or None
+        """
         match_type = None
+
+        patient_status = patient.currentPatientStatus
         if patient_status == 'ON_TREATMENT_ARM':
             match_type = SummaryReport.CURRENT
         elif patient_status == 'PENDING_APPROVAL':
             match_type = SummaryReport.PENDING
+        elif patient_status in Refresher.OTHER_STATUSES:
+            if patient.find_trigger_by_status('ON_TREATMENT_ARM') is not None:
+                match_type = SummaryReport.FORMER
+            elif patient.find_trigger_by_status('PENDING_CONFIRMATION') is not None:
+                match_type = SummaryReport.NOT_ENROLLED
 
-        if match_type is not None:
-            assignment_rec = Refresher._create_assignment_record(pat, patient_status, sum_rpt.treatmentId)
-            sum_rpt.add_patient_by_type(match_type, assignment_rec)
+        return match_type
 
     # def __init__(self, pat_seq_num, ta_version, assnmnt_status, assnmnt_reason, step_num, diseases, analysis_id,
     #              date_selected, date_on_arm, date_off_arm=None):
     @staticmethod
-    def _create_assignment_record(pat, status, ta_id):
+    def _create_assignment_record(patient, ta_id):
         """
 
-        :param pat:
-        :param status:
+        :param patient:
         :param ta_id:
         :return:
         """
-        trigger = pat.find_trigger_by_status(status)
-        ta_version = pat.treatment_arm_version()
+        patient_status = patient.currentPatientStatus
+        trigger = patient.find_trigger_by_status(patient_status)
+        ta_version = patient.treatment_arm_version()
 
         return AssignmentRecord(trigger['patientSequenceNumber'],
                                 ta_version,
-                                pat.currentPatientStatus,
-                                pat.get_assignment_reason(ta_id, ta_version),
-                                pat.currentStepNumber,
-                                pat.diseases,
+                                patient_status,
+                                patient.get_assignment_reason(ta_id, ta_version),
+                                patient.currentStepNumber,
+                                patient.diseases,
                                 "todo: analysisId",  # TODO find out where this comes from
                                 datetime(2001, 1, 1),  # TODO get dateSelected from patientTriggers
                                 trigger['dateCreated'],  # TODO get dateOnArm from patientTriggers
