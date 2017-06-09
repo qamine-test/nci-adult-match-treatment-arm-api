6cf36a8013d0725ebfc7957bad92f1b70b40bf67
Matt Smith <matt.smith2@nih.gov>
2017-06-09 10:46:45 -0400

    Fully unit-tested Patient class.

--- scripts/tests/test_patient.py
+++ scripts/tests/test_patient.py
@@ -5,28 +5,19 @@ from datetime import datetime
 from bson.objectid import ObjectId
 from ddt import ddt, data, unpack
 
-TREATMENT_ARM = {
-    "_id": "EAY131-B",
-    "name": "Afatinib-Her2 activating mutation",
-    "version": "2015-08-06",
-    "description": "Afatinib in HER2 activating mutation",
-    "targetId": "750691.0",
-    "targetName": "Afatinib",
-    "numPatientsAssigned": 0,
-    "maxPatientsAllowed": 35,
-    "treatmentArmStatus": "OPEN",
-}
+from scripts.summary_report_refresher.patient import Patient
 
-def create_default_patient_assignment_logic(trtmtId):
-    return  {
-                "treatmentArmId": trtmtId,
-                "treatmentArmVersion": "2015-08-06",
-                "reason": "The patient contains no matching variant.",
-                "patientAssignmentReasonCategory": "NO_VARIANT_MATCH"
-            }
+# ******** Test Data Constants and Helper Functions to build data structures used in test cases ******** #
 
+def create_default_patient_assignment_logic(trtmt_id):
+    return {
+        "treatmentArmId": trtmt_id,
+        "treatmentArmVersion": "2015-08-06",
+        "reason": "The patient contains no matching variant.",
+        "patientAssignmentReasonCategory": "NO_VARIANT_MATCH"
+    }
 
-TEST_PATIENT = {
+TEST_PATIENT_NO_TA = {
     "_id": ObjectId("55e9e33600929ab89f5499a1"),
     "patientTriggers": [
         {
@@ -57,11 +48,19 @@ TEST_PATIENT = {
         {
             "studyId": "EAY131",
             "patientSequenceNumber": "10065",
+            "stepNumber": "0",
+            "patientStatus": "PENDING_APPROVAL",
+            "dateCreated": datetime(2015, 10, 1, 9, 34, 55),
+            "auditDate": datetime(2015, 10, 1, 9, 34, 56)
+        },
+        {
+            "studyId": "EAY131",
+            "patientSequenceNumber": "10065",
             "stepNumber": "1",
             "patientStatus": "ON_TREATMENT_ARM",
             "message": "Patient registration to assigned treatment arm EAY131-B",
             "dateCreated": datetime(2015, 10, 5, 21, 0, 11),
-            "auditDate": datetime(2015, 10, 5, 21, 0, 11)
+            "auditDate": datetime(2015, 10, 5, 21, 0, 12)
         }
     ],
     "currentStepNumber": "1",
@@ -69,7 +68,7 @@ TEST_PATIENT = {
     "patientAssignments": {
         "dateAssigned": datetime(2015, 9, 29, 21, 00, 11),
         "biopsySequenceNumber": "T-15-000078",
-        "treatmentArm": TREATMENT_ARM,
+        # "treatmentArm": treatment_arm,
         "patientAssignmentStatus": "AVAILABLE",
         "patientAssignmentLogic": [
             {
@@ -116,18 +115,68 @@ TEST_PATIENT = {
             "shortName": "Invasive breast carcinoma"
         }
     ],
-    "treatmentArm": TREATMENT_ARM,
+    # "treatmentArm": treatment_arm,
 }
 
+TREATMENT_ARM = {
+    "_id": "EAY131-B",
+    "name": "Afatinib-Her2 activating mutation",
+    "version": "2015-08-06",
+    "description": "Afatinib in HER2 activating mutation",
+    "targetId": "750691.0",
+    "targetName": "Afatinib",
+    "numPatientsAssigned": 0,
+    "maxPatientsAllowed": 35,
+    "treatmentArmStatus": "OPEN",
+}
 
+TEST_PATIENT = dict(TEST_PATIENT_NO_TA)
+TEST_PATIENT['treatmentArm'] = TREATMENT_ARM
+TEST_PATIENT['patientAssignments']['treatmentArm'] = TREATMENT_ARM
+
+
+# ******** Test the Patient class in patient.py. ******** #
 @ddt
 class TestPatient(unittest.TestCase):
-    @data(
+    def test_constructor_and_get(self):
+        p = Patient(TEST_PATIENT)
+        self.assertEqual(p.diseases, TEST_PATIENT['diseases'])
+        self.assertEqual(p.treatmentArm, TEST_PATIENT['treatmentArm'])
+        self.assertEqual(p.currentPatientStatus, TEST_PATIENT['currentPatientStatus'])
+        self.assertEqual(p.currentStepNumber, TEST_PATIENT['currentStepNumber'])
+        self.assertEqual(p.patientTriggers, TEST_PATIENT['patientTriggers'])
+        self.assertEqual(p.patientAssignments, TEST_PATIENT['patientAssignments'])
 
+    @data(
+        (TEST_PATIENT, TREATMENT_ARM['version']),
+        (TEST_PATIENT_NO_TA, None),
     )
     @unpack
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_treatment_arm_version(self, patient_json, exp_trtmt_ver):
+        patient = Patient(patient_json)
+        self.assertEqual(patient.treatment_arm_version(), exp_trtmt_ver)
+
+    @data(
+        ('PENDING_APPROVAL', TEST_PATIENT['patientTriggers'][3]),
+        ('ON_TREATMENT_ARM', TEST_PATIENT['patientTriggers'][4]),
+        ('STATUS_NOT_FOUND', None),
+    )
+    @unpack
+    def test_find_trigger_by_status(self, status, exp_trigger):
+        p = Patient(TEST_PATIENT)
+        self.assertEqual(p.find_trigger_by_status(status), exp_trigger)
+
+    @data(
+        ('EAY131-Q', '2015-08-06', TEST_PATIENT['patientAssignments']['patientAssignmentLogic'][0]['reason']),
+        ('EAY131-V', '2015-08-06', TEST_PATIENT['patientAssignments']['patientAssignmentLogic'][9]['reason']),
+        ('EAY131-B', '2015-08-06', TEST_PATIENT['patientAssignments']['patientAssignmentLogic'][8]['reason']),
+        ('EAY131-B', '2015-00-00', None),
+        ('EAY131-*', '2015-08-06', None),
+    )
+    @unpack
+    def test_get_assignment_reason(self, treatment_id, treatment_version, exp_reason):
+        p = Patient(TEST_PATIENT)
+        self.assertEqual(p.get_assignment_reason(treatment_id, treatment_version), exp_reason)
 
 
 if __name__ == '__main__':
