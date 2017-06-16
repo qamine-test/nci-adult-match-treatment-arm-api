d40e601609530f12e4dcb5d017b3e00917b7c2d0
Matt Smith <matt.smith2@nih.gov>
2017-06-16 13:42:41 -0400

    Refactored TreatmentArmMessageManager be extracting out SQS access into its own class, SqsAccessor.

---
+++ accessors/sqs_accessor.py
@@ -0,0 +1,25 @@
+import boto3
+
+REGION = 'us-east-1'
+
+class SqsAccessor(object):
+    def __init__(self, queue_name):
+        self.sqs_client = boto3.client('sqs', region_name=REGION)
+        self.queue_name = queue_name
+        response = self.sqs_client.create_queue(QueueName=queue_name)
+        self.queue_url = response['QueueUrl']
+
+    def receive_message(self, attribute_names=None, max_message_cnt=1):
+        if attribute_names is None:
+            attribute_names = []
+        return self.sqs_client.receive_message(QueueUrl=self.queue_url,
+                                               AttributeNames=attribute_names,
+                                               MaxNumberOfMessages=max_message_cnt)
+
+    def delete_message(self, message):
+        self.sqs_client.delete_message(QueueUrl=self.queue_url,
+                                       ReceiptHandle=message['ReceiptHandle'])
+
+    def send_message(self, message_body):
+        return self.sqs_client.send_message(QueueUrl=self.queue_url,
+                                            MessageBody=message_body)
