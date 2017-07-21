525b9bcbc48f6033318ae18ea4b159ed9928a8b8
Matt Smith <matt.smith2@nih.gov>
2017-07-21 16:34:35 -0400

    Commented out the settings that should now be set/get in the Environment object.

--- config/flask_config.py
+++ config/flask_config.py
@@ -1,5 +1,3 @@
-import os
-
 from flask_env import MetaFlaskEnv
 
 
@@ -7,8 +5,10 @@ class Configuration(metaclass=MetaFlaskEnv):
     """
     Service configuration
     """
-    DEBUG = True
-    PORT = 5010
-    MONGODB_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Match')
-    # Some instances of the DB are named 'Match' and others 'match'.
-    DB_NAME = 'match' if '/match' in MONGODB_URI else 'Match'
+    DEBUG = True   # For some reason, setting this to True causes JSON to be "pretty-printed"
+
+    # Commented out because these are now retrieved from the helpers.environment.Environment class.
+    # PORT = 5010
+    # MONGODB_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Match')
+    # # Some instances of the DB are named 'Match' and others 'match'.
+    # DB_NAME = 'match' if '/match' in MONGODB_URI else 'Match'
