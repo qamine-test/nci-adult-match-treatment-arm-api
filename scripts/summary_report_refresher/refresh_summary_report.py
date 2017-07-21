156a8b346cef1bd77a8883af272829240c688d5d
Matt Smith <matt.smith2@nih.gov>
2017-07-21 16:40:44 -0400

    Got rid of flask.  Calls the new log_config() function from the log module.

--- scripts/summary_report_refresher/refresh_summary_report.py
+++ scripts/summary_report_refresher/refresh_summary_report.py
@@ -5,26 +5,28 @@ The primary use of this script is for development.  (Ordinarily, it will be exec
 """
 import logging
 
-from flask import Flask
-
-from config.flask_config import Configuration
-from config import log  # pylint: disable=unused-import
+# from flask import Flask
+#
+# from config.flask_config import Configuration
+from config import log
 from scripts.summary_report_refresher.refresher import Refresher
 
 # Logging functionality
+log.log_config()
 LOGGER = logging.getLogger(__name__)
 
-APP = Flask(__name__)
-APP.config.from_object(Configuration)
-
+# APP = Flask(__name__)
+# APP.config.from_object(Configuration)
+#
 
 if __name__ == '__main__':
     LOGGER.info("Starting the Summary Report Refresher")
 
     exit_code = 0
     try:
-        with APP.app_context():
-            Refresher().run()
+        # with APP.app_context():
+        r = Refresher()
+        r.run()
     except Exception as exc:
         LOGGER.exception(str(exc))
         exit_code = 1
