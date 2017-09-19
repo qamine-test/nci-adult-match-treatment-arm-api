bbf910d0df21347f29f351398b3dc7ae390385be
Matt Smith <matt.smith2@nih.gov>
2017-09-19 10:05:33 -0400

    New unit tests for the PatientAccessor class.

---
+++ tests/test_patient_accessor.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import unittest
+from mock import patch, Mock
+
+from accessors.patient_accessor import PatientAccessor
+
+
+TEST_PATIENT_API_URL = "http://my/test/patient_api"
+
+
+class PatientAccessorTests(unittest.TestCase):
+    def setUp(self):
+        env_patcher = patch('accessors.patient_accessor.Environment')
+        self.addCleanup(env_patcher.stop)
+        self.mock_env = env_patcher.start().return_value
+        self.mock_env.patient_api_url = TEST_PATIENT_API_URL
+
+        logging_patcher = patch('accessors.patient_accessor.logging')
+        self.addCleanup(logging_patcher.stop)
+        self.mock_logger = logging_patcher.start().getLogger()
+
+    def test_constructor(self):
+        self.assertEqual(PatientAccessor().url, TEST_PATIENT_API_URL+'/by_treatment_arm')
+
+    @patch('accessors.patient_accessor.requests')
+    def test_get_patients_by_treatment_arm_id(self, mock_requests):
+        # Doesn't really matter what the treatment arm ID is or what patients are returned; just verifying here that
+        # the service is called correctly and the returned data is handled correctly.
+        treatment_id = "TA_ID_5"
+        patients = [{'patientSequenceNumber': 1}, {'patientSequenceNumber': 2}, {'patientSequenceNumber': 3}]
+
+        mock_response = Mock()
+        mock_response.json.return_value = patients
+        mock_requests.get.return_value = mock_response
+
+        patient_accessor = PatientAccessor()
+        result = patient_accessor.get_patients_by_treatment_arm_id(treatment_id)
+
+        mock_requests.get.assert_called_once_with(TEST_PATIENT_API_URL+'/by_treatment_arm/'+treatment_id)
+        mock_response.json.assert_called_once_with()
+        self.assertEqual(result, patients)
+
+if __name__ == '__main__':
+    unittest.main()
