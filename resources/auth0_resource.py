857b91aec4067307616541cd6c342fe92a36c8c4
Matt Smith <matt.smith2@nih.gov>
2017-09-21 17:11:33 -0400

    Refacotred the command the creates the response for an error.

--- resources/auth0_resource.py
+++ resources/auth0_resource.py
@@ -1,4 +1,4 @@
-import base64
+# import base64
 import os
 from functools import wraps
 
@@ -13,15 +13,8 @@ from werkzeug.local import LocalProxy
 current_user = LocalProxy(lambda: _request_ctx_stack.top.current_user)
 
 
-# Authentication attribute/annotation
-def authenticate(error):  # pragma: no cover
-    response = jsonify(error)
-    response.status_code = 401
-    return response
-
 def error_response(code, description):  # pragma: no cover
-    error = {'code': code, 'description': description}
-    response = jsonify(error)
+    response = jsonify({'code': code, 'description': description})
     response.status_code = 401
     return response
 
@@ -39,8 +32,7 @@ def requires_auth(function):  # pragma: no cover
             auth = request.headers.get('Authorization', None)
             # print(str(auth))
             if not auth:
-                return authenticate(
-                    {'code': 'authorization_header_missing', 'description': 'Authorization header is expected'})
+                return error_response('authorization_header_missing', 'Authorization header is expected')
 
             parts = auth.split()
 
@@ -51,54 +43,36 @@ def requires_auth(function):  # pragma: no cover
             elif len(parts) > 2:
                 return {'code': 'invalid_header', 'description': 'Authorization header must be Bearer + \s + token'}
             token = parts[1]
-            # auth0_client_secret = base64.b64decode(
-            #     os.environ['AUTH0_CLIENT_SECRET'].replace("_", "/").replace("-", "+"))
+
             try:
                 auth0_client_secret = os.environ['AUTH0_CLIENT_SECRET']
             except KeyError:
-                return authenticate({'code': 'missing secret',
-                                     'description': 'Environment variable AUTH0_CLIENT_SECRET is missing'})
+                return error_response('missing secret', 'Environment variable AUTH0_CLIENT_SECRET is missing')
 
-            # print("token='{}'".format(token))
-            # print("auth0_client_secret='{}'".format(auth0_client_secret))
-            auth0_client_secret = auth0_client_secret if len(auth0_client_secret) == 64 else  base64.b64decode(
-                auth0_client_secret)
-            # print("DECODED auth0_client_secret='{}'".format(auth0_client_secret))
+            # auth0_client_secret = auth0_client_secret if len(auth0_client_secret) == 64 else  base64.b64decode(
+            #     auth0_client_secret)
 
             try:
                 auth0_client_id = os.environ['AUTH0_CLIENT_ID']
             except KeyError:
-                return authenticate({'code': 'missing client ID',
-                                     'description': 'Environment variable AUTH0_CLIENT_ID is missing'})
-            # print("auth0_client_id='{}'".format(auth0_client_id))
+                return error_response('missing client ID', 'Environment variable AUTH0_CLIENT_ID is missing')
+
             try:
                 payload = jwt.decode(token, auth0_client_secret, audience=auth0_client_id)
             except jwt.ExpiredSignature:
-                return authenticate({'code': 'token_expired', 'description': 'token is expired'})
+                return error_response('token_expired', 'token is expired')
             except jwt.InvalidAudienceError:
-                return authenticate({'code': 'invalid_audience', 'description': 'incorrect audience, expected: '})
+                return error_response('invalid_audience', 'incorrect audience')
             except jwt.DecodeError:
-                return authenticate({'code': 'token_invalid_signature', 'description': 'token signature is invalid'})
+                return error_response('token_invalid_signature', 'token signature is invalid')
             except Exception as e:
-                return authenticate({'code': 'Unknown exception raised', 'description': str(e)})
+                return error_response('Unknown_exception_raised', str(e))
 
-            print("payload='{}'".format(payload))
             # Check for proper parameters in token.
-            # try:
-            #     payload['roles']
-            # except KeyError:
-            #     return authenticate(
-            #         {'code': 'token_incomplete', 'description': 'token must contain roles and email in scope!'})
-            # try:
-            #     payload['email']
-            # except KeyError:
-            #     return authenticate(
-            #         {'code': 'token_incomplete', 'description': 'token must contain roles and email in scope!'})
             if 'roles' not in payload or 'email' not in payload:
-                return authenticate(
-                    {'code': 'token_incomplete', 'description': 'token must contain roles and email in scope!'})
+                return error_response('token_incomplete', 'token must contain roles and email in scope!')
 
-            print("Executing function")
+            # print("Executing function")
             return function(*args, **kwargs)
 
         return decorated
