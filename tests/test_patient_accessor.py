7eedfc5cd9ca114ee4f9cfcd2c1305162ec4d7cf
Matt Smith <matt.smith2@nih.gov>
2017-09-25 08:46:50 -0400

    updated test for PatientAccessor.get_patients_by_treatment_arm_id() to validate authorization parameter is used correctly.

--- tests/test_patient_accessor.py
+++ tests/test_patient_accessor.py
@@ -2,6 +2,7 @@
 
 import unittest
 from mock import patch, Mock
+from ddt import ddt, data, unpack
 
 from accessors.patient_accessor import PatientAccessor
 
@@ -9,6 +10,7 @@ from accessors.patient_accessor import PatientAccessor
 TEST_PATIENT_API_URL = "http://my/test/patient_api"
 
 
+@ddt
 class PatientAccessorTests(unittest.TestCase):
     def setUp(self):
         env_patcher = patch('accessors.patient_accessor.Environment')
@@ -23,8 +25,13 @@ class PatientAccessorTests(unittest.TestCase):
     def test_constructor(self):
         self.assertEqual(PatientAccessor().url, TEST_PATIENT_API_URL+'/by_treatment_arm')
 
+    @data(
+        (None, {}),
+        ("Bearer ThisIsMyFakeAuth0TokenId", {"authorization": "Bearer ThisIsMyFakeAuth0TokenId"})
+    )
+    @unpack
     @patch('accessors.patient_accessor.requests')
-    def test_get_patients_by_treatment_arm_id(self, mock_requests):
+    def test_get_patients_by_treatment_arm_id(self, authorization_token, exp_headers, mock_requests):
         # Doesn't really matter what the treatment arm ID is or what patients are returned; just verifying here that
         # the service is called correctly and the returned data is handled correctly.
         treatment_id = "TA_ID_5"
@@ -35,9 +42,10 @@ class PatientAccessorTests(unittest.TestCase):
         mock_requests.get.return_value = mock_response
 
         patient_accessor = PatientAccessor()
-        result = patient_accessor.get_patients_by_treatment_arm_id(treatment_id)
+        result = patient_accessor.get_patients_by_treatment_arm_id(treatment_id, authorization_token)
 
-        mock_requests.get.assert_called_once_with(TEST_PATIENT_API_URL+'/by_treatment_arm/'+treatment_id)
+        exp_url = TEST_PATIENT_API_URL + '/by_treatment_arm/' + treatment_id
+        mock_requests.get.assert_called_once_with(exp_url, headers=exp_headers)
         mock_response.json.assert_called_once_with()
         self.assertEqual(result, patients)
 
