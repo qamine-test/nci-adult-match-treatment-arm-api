1f7eaf45b6a39d2b693dd9d857d77c868d574c34
Matt Smith <matt.smith2@nih.gov>
2017-10-18 17:47:14 -0400

    Added a try/except block to give better messages about success or failure to update.

--- scripts/ta_message_manager/update_refresh_lambda.py
+++ scripts/ta_message_manager/update_refresh_lambda.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import pprint
 import zipfile
 from io import BytesIO
@@ -8,19 +9,26 @@ import boto3
 
 from helpers.environment import Environment
 
-# Zip up the code for the lambda function in memory
-buffer = BytesIO()
-with zipfile.ZipFile(buffer, 'w') as lambda_zip:
-    lambda_zip.write('send_refresh_message.py')
-buffer.seek(0)
-zip_data = buffer.read()
+try:
+    # Zip up the code for the lambda function in memory
+    buffer = BytesIO()
+    with zipfile.ZipFile(buffer, 'w') as lambda_zip:
+        lambda_function_script = 'scripts/ta_message_manager/send_refresh_message.py'
+        lambda_zip.write(os.path.abspath(lambda_function_script), os.path.basename(lambda_function_script))
+    buffer.seek(0)
+    zip_data = buffer.read()
 
-# Send the zipped function to AWS
-client = boto3.client('lambda', region_name=Environment().region)
-response = client.update_function_code(
-    FunctionName='SendSummaryReportRefreshMessage',
-    Publish=True,
-    ZipFile=zip_data,
-)
+    # Send the zipped function to AWS
+    client = boto3.client('lambda', region_name=Environment().region)
+    response = client.update_function_code(
+        FunctionName='SendSummaryReportRefreshMessage',
+        Publish=True,
+        ZipFile=zip_data,
+    )
 
-pprint.pprint(response)
+    print("SUCCESS!  Summary Report Refresh Lambda function has been updated.\nThis does not necessarily mean "
+          "that the function works; that still needs to be tested.\n\nThe response from the update:")
+    pprint.pprint(response)
+except Exception as e:
+    print("An exception prevented the successful upload of the new Lambda function:\n\n{}\n".format(str(e)))
+    exit(16)
