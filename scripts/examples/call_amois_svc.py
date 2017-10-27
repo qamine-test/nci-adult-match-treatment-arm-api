745ce018dbd3ab758abd93a33da153ce972ea1d6
Matt Smith <matt.smith2@nih.gov>
2017-10-27 14:07:55 -0400

    IsAmoisResource.get() method has been renamed to IsAmoisResource.patch().

--- scripts/examples/call_amois_svc.py
+++ scripts/examples/call_amois_svc.py
@@ -93,7 +93,7 @@ if 'TOKEN_ID' in os.environ:
     print("")
 
     is_amoi_json = {"type": "singleNucleotideVariants", "variants": vr['singleNucleotideVariants']}
-    resp = requests.get('http://localhost:5010/api/v1/treatment_arms/is_amoi', json=is_amoi_json, headers=headers)
+    resp = requests.patch('http://localhost:5010/api/v1/treatment_arms/is_amoi', json=is_amoi_json, headers=headers)
     pprint.pprint(resp.json())
     print("")
 
