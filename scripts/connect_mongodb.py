aa60c4e51e5b3f8c1c45e44f543e27cbd7710ae1
Matt Smith <matt.smith2@nih.gov>
2017-11-06 15:58:45 -0500

    Added exception handling to connection testing script.

--- scripts/connect_mongodb.py
+++ scripts/connect_mongodb.py
@@ -1,15 +1,63 @@
-from pymongo import MongoClient
 import os
+import ssl
 import sys
+import traceback
+
+from pymongo import MongoClient
 
 uri = os.environ.get('MONGODB_URI', None)
 if uri is None:
     print("Required MONGODB_URI Env Var is missing.")
     sys.exit(64)
 
-mongo_client = MongoClient(uri)
-database = mongo_client['Match']
-collection = database['treatmentArms']
-count = collection.count({})
-print("{} arms in treatmentArms".format(count))
-collection.find({ 'dateArchived': None }, )
+try:
+    mongo_client = MongoClient(uri)
+    # mongo_client = MongoClient(uri, ssl_cert_reqs=ssl.CERT_NONE)
+except Exception as e:
+    print("Exception caught during connection.")
+    print(str(e))
+    sys.exit(128)
+
+try:
+    database = mongo_client['Match']
+except Exception as e:
+    print(traceback.format_exc())
+    print("Exception caught getting Match database.")
+    print(str(e))
+    sys.exit(128)
+
+try:
+    collection = database['treatmentArms']
+except Exception as e:
+    print(traceback.format_exc())
+    print("Exception caught getting treatmentArms collection.")
+    print(str(e))
+    sys.exit(128)
+
+try:
+    count = collection.count({})
+except Exception as e:
+    print(traceback.format_exc())
+    print("Exception caught getting treatmentArms count.")
+    print(str(e))
+    sys.exit(128)
+else:
+    print("{} arms in treatmentArms".format(count))
+
+try:
+    count = database['treatmentArm'].count({})
+except Exception as e:
+    print("Exception caught getting treatmentArm count.")
+    print(str(e))
+    sys.exit(128)
+else:
+    print("{} arms in treatmentArm".format(count))
+
+try:
+    count = database['treatmentArmHistory'].count({})
+except Exception as e:
+    print("Exception caught getting treatmentArmHistory count.")
+    print(str(e))
+    sys.exit(128)
+else:
+    print("{} arms in treatmentArmHistory".format(count))
