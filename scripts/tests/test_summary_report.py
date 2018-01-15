69c419b2c5c4a82163292e9ca0dfd00f7cab2c6d
Matt Smith <matt.smith2@nih.gov>
2018-01-15 15:57:48 -0500

    Tested the assignment of 'slot' to the assignment records.

--- scripts/tests/test_summary_report.py
+++ scripts/tests/test_summary_report.py
@@ -22,6 +22,7 @@ ALL_FIELDS = SummaryReport._REQ_JSON_FIELDS
 PAT_SEQ_NUM = '112244'
 PATIENT_TYPE = 'STANDARD'
 TA_VERSION = '2016-04-25'
+BIOPSY_SEQ_NUM = 'BSN-112244'
 ASSNMNT_STATUS = 'ON_TREATMENT_ARM'
 ASSNMNT_REASON = 'Because I said so'
 STEP_NUM = 1
@@ -43,9 +44,15 @@ DATE_OFF_ARM = datetime(2016, 5, 1)
 
 # ******** Helper functions to build data structures used in test cases ******** #
 
-def create_assignment_rec():
+def create_assignment_rec(date_selected=None, date_on_arm=None, date_off_arm=None):
     return AssignmentRecord(PAT_SEQ_NUM, PATIENT_TYPE, TA_VERSION, ASSNMNT_STATUS, ASSNMNT_REASON, STEP_NUM,
-                            DISEASES, ASSNMNT_IDX, ANALYSIS_ID, DATE_SEL, DATE_ON_ARM, None)
+                            DISEASES, ASSNMNT_IDX, ANALYSIS_ID, BIOPSY_SEQ_NUM,
+                            date_selected, date_on_arm, date_off_arm)
+
+PENDING_ASSMT_REC = create_assignment_rec(date_selected=DATE_SEL, date_on_arm=None, date_off_arm=None)
+NOT_ENROLLED_ASSMT_REC = create_assignment_rec(date_selected=DATE_SEL, date_on_arm=None, date_off_arm=DATE_OFF_ARM)
+CURRENTLY_ON_TA_ASSMT_REC = create_assignment_rec(date_selected=DATE_SEL, date_on_arm=DATE_ON_ARM, date_off_arm=None)
+FORMERLY_ON_TA_AR = create_assignment_rec(date_selected=DATE_SEL, date_on_arm=DATE_ON_ARM, date_off_arm=DATE_OFF_ARM)
 
 
 def create_ta_json(omit_flds=None):
@@ -102,17 +109,22 @@ class TestSummaryReport(unittest.TestCase):
         cls.test_sr = SummaryReport(create_ta_json())
 
     @data(
-        (SummaryReport.NOT_ENROLLED, 1, 0, 0, 0),
-        (SummaryReport.NOT_ENROLLED, 2, 0, 0, 0),
-        (SummaryReport.CURRENT, 2, 0, 1, 0),
-        (SummaryReport.PENDING, 2, 0, 1, 1),
-        (SummaryReport.CURRENT, 2, 0, 2, 1),
-        (SummaryReport.CURRENT, 2, 0, 3, 1),
-        (SummaryReport.FORMER, 2, 1, 3, 1),
+        (SummaryReport.NOT_ENROLLED, NOT_ENROLLED_ASSMT_REC, 1, 0, 0, 0),
+        (SummaryReport.NOT_ENROLLED, NOT_ENROLLED_ASSMT_REC, 2, 0, 0, 0),
+        (SummaryReport.CURRENT, CURRENTLY_ON_TA_ASSMT_REC, 2, 0, 1, 0),
+        (SummaryReport.PENDING, PENDING_ASSMT_REC, 2, 0, 1, 1),
+        (SummaryReport.CURRENT, CURRENTLY_ON_TA_ASSMT_REC, 2, 0, 2, 1),
+        (SummaryReport.CURRENT, CURRENTLY_ON_TA_ASSMT_REC, 2, 0, 3, 1),
+        (SummaryReport.FORMER, FORMERLY_ON_TA_AR, 2, 1, 3, 1),
     )
     @unpack
-    def test_normal_add(self, patient_type, not_enrolled_cnt, former_cnt, current_cnt, pending_cnt):
-        self.test_sr.add_patient_by_type(patient_type, create_assignment_rec())
+    def test_counters_after_normal_add(self, patient_type, assmt_rec, not_enrolled_cnt,
+                                       former_cnt, current_cnt, pending_cnt):
+        """
+        This test just verifies the expected value of the counter fields of the SummaryReport object.
+        The assignmentRecords field is confirmed elsewhere.
+        """
+        self.test_sr.add_patient_by_type(patient_type, assmt_rec)
         self.assertEqual(self.test_sr.numNotEnrolledPatient, not_enrolled_cnt)
         self.assertEqual(self.test_sr.numPendingArmApproval, pending_cnt)
         self.assertEqual(self.test_sr.numFormerPatients, former_cnt)
@@ -124,6 +136,50 @@ class TestSummaryReport(unittest.TestCase):
         exc_str = str(cm.exception)
         self.assertEqual(exc_str, "Invalid patient type 'invalidType'")
 
+    @data(
+        # 1.  Not selected for arm (shouldn't ever happen in real world scenario because of how patients are
+        #     pulled from the database)
+        (create_assignment_rec(date_selected=None, date_on_arm=None, date_off_arm=None), False),
+        # 2.  Patient in PENDING_CONFIRMATION or PENDING_APPROVAL
+        (PENDING_ASSMT_REC, True),
+        # 3.  Patient not enrolled (selected but never put on arm)
+        (NOT_ENROLLED_ASSMT_REC, False),
+        # 4.  Patient currently on treatment arm
+        (CURRENTLY_ON_TA_ASSMT_REC, True),
+        # 5.  Patient formerly on treatment arm
+        (FORMERLY_ON_TA_AR, True),
+    )
+    @unpack
+    def test_assignment_occupies_slot(self, assignment_record, exp_result):
+        result = SummaryReport._assignment_occupies_slot(assignment_record.get_json())
+        self.assertEqual(result, exp_result)
+
+
+    @data(
+        # 1. Test get_json immediately after construction
+        ([], DEFAULT_SR),
+        # 2. Test get_json after three patients added
+        ([(SummaryReport.NOT_ENROLLED, NOT_ENROLLED_ASSMT_REC),
+          (SummaryReport.CURRENT, CURRENTLY_ON_TA_ASSMT_REC),
+          (SummaryReport.PENDING, PENDING_ASSMT_REC),
+          ],
+         {'assignmentRecords': [NOT_ENROLLED_ASSMT_REC.get_json(),
+                                dict(**CURRENTLY_ON_TA_ASSMT_REC.get_json(), **{"slot": 1}),
+                                dict(**PENDING_ASSMT_REC.get_json(), **{"slot": 2})],
+          'numNotEnrolledPatient': 1, 'numFormerPatients': 0, 'numCurrentPatientsOnArm': 1, 'numPendingArmApproval': 1})
+    )
+    @unpack
+    def test_get_json(self, test_patients, exp_result):
+        sr = SummaryReport(create_ta_json())
+        for patient_type, assignment_record in test_patients:
+            # print("Adding patient with assignment record type = {}".format(type(assignment_record)))
+            sr.add_patient_by_type(patient_type, assignment_record)
+
+        result = sr.get_json()
+        self.maxDiff = None
+        self.assertEqual(result, exp_result)
+
 
 if __name__ == '__main__':
+    print(str(DEFAULT_SR))
     unittest.main()
