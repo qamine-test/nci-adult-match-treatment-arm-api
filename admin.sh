8c1de62776146ce61beea15737afd297bdd60e3e
Mark Hanna <hannama@nih.gov>
2018-04-10 16:26:42 -0400

    Adding admin.sh entry to .travis.yml

---
+++ admin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+gem install travis
+pip install 'requests[security]'
+pip install --user awscli
+export BUCKETNAME="publictoprivate"
+export ENVFILENAME=`echo $TRAVIS_REPO_SLUG|sed -e 's/BIAD\///'`
+travis login --github-token $GITHUB_TOKEN --org
+travis env list -r $TRAVIS_REPO_SLUG|sed -e 's/=.*$//' > TRAVLIST
+travis logout
+env > env.txt
+for var in `cat TRAVLIST|grep -v ^#`;do
+    grep $var env.txt|sed -e 's/=/ /'
+done >> $ENVFILENAME
+aws s3 cp $ENVFILENAME s3://$BUCKETNAME --region=us-east-1
